09c904020955ef5647736436527f0daa55685036
pavlov95 <dim_pavlov@abv.bg>
2026-07-03 14:07:02 +0300

    feat: add role-based permissions

--- src/main/java/tournament_trail/demo/security/SecurityConfiguration.java
+++ src/main/java/tournament_trail/demo/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package tournament_trail.demo.security;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,9 +15,9 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                PathRequest.toStaticResources().atCommonLocations())
                         .permitAll()
                         .requestMatchers(
                                 "/",
@@ -25,12 +26,30 @@ public class SecurityConfiguration {
                                 "/login",
                                 "/register",
                                 "/verify",
-                                "/check-your-email")
+                                "/check-your-email",
+                                "/error")
+                        .permitAll()
+                        .requestMatchers(
+                                HttpMethod.POST,
+                                "/tournaments")
+                        .hasAnyRole("ORGANISER", "ADMIN")
+                        .requestMatchers(
+                                HttpMethod.PUT,
+                                "/tournaments/*")
+                        .hasAnyRole("ORGANISER", "ADMIN")
+                        .requestMatchers(
+                                HttpMethod.DELETE,
+                                "/tournaments/*")
+                        .hasAnyRole("ORGANISER", "ADMIN")
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/tournaments",
+                                "/tournaments/*")
                         .permitAll()
                         .requestMatchers("/admin/**")
                         .hasRole("ADMIN")
-                        .requestMatchers("/organizer/**")
-                        .hasAnyRole("ORGANIZER", "ADMIN")
+                        .requestMatchers("/organiser/**")
+                        .hasAnyRole("ORGANISER", "ADMIN")
                         .anyRequest()
                         .authenticated()
                 )
@@ -38,14 +57,11 @@ public class SecurityConfiguration {
                         .loginPage("/login")
                         .defaultSuccessUrl("/home", true)
                         .failureUrl("/login?error")
-                        .permitAll()
-                )
+                        .permitAll())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/")
-                        .permitAll()
-                );
-
+                        .permitAll());
         return http.build();
     }
 }
