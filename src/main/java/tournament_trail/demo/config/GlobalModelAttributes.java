5ca81b0ed4fbf85e943b2248a49bb1b37d5e2cbc
pavlov95 <dim_pavlov@abv.bg>
2026-08-22 23:08:27 +0300

    feat: add i18n support with English and Bulgarian messages

---
+++ src/main/java/tournament_trail/demo/config/GlobalModelAttributes.java
@@ -0,0 +1,14 @@
+package tournament_trail.demo.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalModelAttributes {
+
+    @ModelAttribute("currentUri")
+    public String currentUri(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+}
